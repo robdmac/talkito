77ba6c829f1af80c4cb298f3dca590f5dd7c3dd4
Rob Macrae <rob.d.macrae@gmail.com>
2026-01-05 00:02:03 -0300

    Fix SSL bug: use pip directly with --trusted-host

--- install.sh
+++ install.sh
@@ -96,11 +96,9 @@ install_talkito() {
     # Activate virtual environment
     source venv/bin/activate
     
-    # Upgrade pip using get-pip.py with trusted hosts (avoids SSL bug in Python 3.10)
+    # Upgrade pip with trusted hosts (avoids SSL bug in Python 3.10)
     echo "   Upgrading pip..."
-    curl -sS https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-    python3 get-pip.py --quiet --trusted-host pypi.org --trusted-host files.pythonhosted.org
-    rm get-pip.py
+    pip install --upgrade pip --trusted-host pypi.org --trusted-host files.pythonhosted.org
 
     # Install talkito (all features included by default)
     echo "   Installing talkito..."
